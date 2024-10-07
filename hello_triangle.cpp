2dbc2fe20c5eba8cf1d1e7e67d7e2257e7071d49
andrei <andrei.ghita2002@gmail.com>
2024-10-07 17:45:15 +0100

    added validation layers to Vulkan

--- hello_triangle.cpp
+++ hello_triangle.cpp
@@ -4,6 +4,40 @@
 #include <vector>
 #include "hello_triangle.h"
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+// validation layers:
+const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound) return false;
+    }
+    return true;
+}
+
 void hello_triangle::initWindow() {
     glfwInit();
     // Tell GLFW not to use OpenGL.
@@ -11,7 +45,7 @@ void hello_triangle::initWindow() {
     // Tell GLFW that the window shouldn't be resizable.
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     // Creating the window
-    window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Hello", nullptr, nullptr);
 }
 
 void hello_triangle::initVulkan() {
@@ -45,6 +79,22 @@ void hello_triangle::initVulkan() {
     createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
     createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
     createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    // validation layers
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "Simplify"
+    if (enableValidationLayers) {
+        if (!checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
+#pragma clang diagnostic pop
+    // TODO define a callback function:
+    // https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Validation_layers#page_Message-callback
 
     // creating the instance
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
